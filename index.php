b81644da53f8f438f90fca33cd1a5e0a69307ddf
Audrey Hayes <audreyhayes0@gmail.com>
2019-02-02 16:05:11 +0000

    included test routes in index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
-//require "./tests/validation.php";
-require "./models/Fence.php";
-require "./views/index.view.php";
+include_once './http/Request.php';
+include_once './http/Router.php';
+include_once "./models/Fence.php";
+include_once "./views/index.view.php";
+
+$router = new Router(new Request);
+
+$router->get('/', function() {
+  return <<<HTML
+  <h1>Hello world</h1>
+HTML;
+});
+$router->get('/profile', function($request) {
+  return <<<HTML
+  <h1>Profile</h1>
+HTML;
+});
+$router->post('/data', function($request) {
+  return json_encode($request->getBody());
+});
