55ea3e12edf817f90342f1fee01796246f49794d
Audrey Hayes <audreyhayes0@gmail.com>
2019-02-02 14:02:59 +0000

    change page title

--- views/view.php
+++ views/view.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Posts & Railings Challenge</title>
+        <title>Fence Fancy Challenge</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">  
@@ -14,7 +14,7 @@
     <body>
         <header>
             <h1>Fence Fancy!</h1>
-            <h2>Calculate how many materials you need to build a fence of a certain minimum length OR provide a quantity of materials and find out the maximum length fence you can build.</h2>
+            <h2>A PHP application that can calculate the amount of posts and railings required to build a fence of a certain length or that will calculate the length of a fence when you provide the amount of posts and railings. </h2>
         </header>
         <form action="index.php" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <h3>OPTION 1: How many posts and railings do you have?</h3>
