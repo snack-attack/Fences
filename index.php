14a321a8f84dad0bd61f9a5442e31149846f3a54
Audrey Hayes <audreyhayes0@gmail.com>
2019-02-03 13:31:44 +0000

    cleaned up comments in index

--- index.php
+++ index.php
@@ -7,23 +7,9 @@ include_once "./views/index.view.php";
 
 $router = new Router(new Request);
 
-// $router->get('/', function() {
-// return <<<HTML
-// <h1>Hello world</h1>
-// HTML;
-// });
-
-// $router->get('/fence', function($request) {
-// return <<<HTML
-// <h1>Profile</h1>
-// HTML;
-// });
-
 $router->post('/index.php', function($request) {
     $request = $request->getBody();
     $fence = new Fence();
     $length = $fence->setLength($request['length']);
-    //$length = $fence->setLength();
-    //$posts = $fence->setPosts($request['posts']);
     var_dump($length);
 });
