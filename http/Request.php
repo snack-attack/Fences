7619be80078f3580ea4dd408b3888bb56ca389a3
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-05 21:00:08 +0000

    fixed getbody() method

--- http/Request.php
+++ http/Request.php
@@ -35,15 +35,13 @@ class Request implements IRequest {
         }
 
         if ($this->requestMethod == "POST") {
-            $result = array();
+            $body = array();
         
             foreach($_POST as $key => $value) {
-                $result[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
 
-            return $result;
+            return $body;
         }
-
-    return $body;
     }
 }
