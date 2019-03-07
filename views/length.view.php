7cbb50f50af66cabd25ffacaacf0aec9e7d1d98a
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-07 19:53:13 +0000

    tried including index.php to access variables, but no go

--- views/length.view.php
+++ views/length.view.php
@@ -1,7 +1,7 @@
 <section>
     <form method="post"><br>
-        <h2>The minimum length of fence you require:</h2>
-        Length: <input type="number" name="length" min="0"><br><br>
+        <h2>What is the minimum fence length you require?</h2>
+        Length: <input type="number" name="length" min="0" step="0.01"><br><br>
         <button name="submit">Build me a fence!</button>
     </form>
 </section>
