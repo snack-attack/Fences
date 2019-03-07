567355e3dd3259bda70cf58e8949dd05af7e4050
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-07 16:10:10 +0000

    moved call to view inside the controller function

--- index.php
+++ index.php
@@ -3,25 +3,22 @@
 require_once './http/Request.php';
 require_once './http/Router.php';
 require_once './models/Fence.php';
-require_once './views/index.view.php';
 
 $router = new Router(new Request);
 
-$router->post('/index.php', function($request) {
+$router->post('/', function($request) {
+    require_once './views/index.view.php';
+
     $request = $request->getBody();
     $fence = new Fence();
-
+    
     $fence->setLength($request['length']);
     $fence->setPosts($request['posts']);
     $fence->setRailings($request['railings']);
-
+    
     $length = $fence->getLength();
     $posts = $fence->getPosts();
     $railings = $fence->getRailings();
-
-    var_dump($railings);
-    var_dump($posts);
-    var_dump($length);
 });
 
 $router->get('/', function() {
